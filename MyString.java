a9c7055c728897fb5fc6fbdfc576daedf5f58fcd
Simon Levy <simon.levy01@post.runi.ac.il>
2024-12-02 00:24:39 +0200

    finished assignment 3

--- MyString.java
+++ MyString.java
@@ -18,13 +18,36 @@ public class MyString {
 
     /** Returns the lowercase version of the given string. */
     public static String lowerCase(String str) {
-        // Replace the following statement with your code
-        return null;
+        String lower = ""; //initialize empty string
+        for (int i=0 ; i<str.length(); i++){ //loop over str
+            if(str.charAt(i)>='A' && str.charAt(i)<='Z'){ //if uppercase
+                lower += (char) (str.charAt(i)+32); //convert to lower and add
+            } else { //if lowercase
+                lower += str.charAt(i); //concatenate to string
+            }
+        }
+        return lower;
     }
 
     /** If str1 contains str2, returns true; otherwise returns false. */
     public static boolean contains(String str1, String str2) {
-        // Replace the following statement with your code
+        if (str1.length() == 0 || str2.length()>str1.length()){ //if str1 is empty or str2>str1
+            return false;
+        } else if (str2.length() == 0){ //if str2 empty
+            return true;
+        }
+        for (int i = 0 ; i<=str1.length()-str2.length() ; i++){ //loop over str1 until there sint space for str2
+            Boolean match = true;
+            for (int j =0 ; j<str2.length() ; j++){ // loop over str2
+                if (str1.charAt(i+j) != str2.charAt(j)){ //checks for mismatch. if non, will stay true
+                    match = false;
+                    break; //exit inner loop 
+                }
+            }
+            if (match){
+                return true;
+            }
+        }
         return false;
     }
 }
--- Primes.java
+++ Primes.java
@@ -1,14 +1,14 @@
 public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
-        Boolean [] arr = new Boolean[n+1];
-        for (int i = 2 ; i< n+1 ; i++){
+        Boolean [] arr = new Boolean[n+1]; //create new array
+        for (int i = 2 ; i< n+1 ; i++){ //make all values past index 1 true
             arr[i] = true;
         }
-        for (int i = 2 ; i< Math.sqrt(n+1) ; i++){
+        for (int i = 2 ; i< Math.sqrt(n+1) ; i++){ // check if the given index is true(i.e. prime)
             if (arr[i]==true){
-                for (int j = i*i ; j< n+1 ; j+=i){
-                    arr[j] = false;
+                for (int j = i*i ; j< n+1 ; j+=i){ // go through all multiples of i 
+                    arr[j] = false; //change all values at multiples of i to false
                 }
             }
         }
@@ -16,11 +16,11 @@ public class Primes {
         int count = 0;
         for (int i = 2 ; i< n+1 ; i++){
             if (arr[i]==true){
-                System.out.println(i);
-                count ++;
+                System.out.println(i); // print out every prime
+                count ++; //coutn amoutn of primes
             } 
         }
-        int percent = (int)(100.0*count/n);
+        int percent = (int)(100.0*count/n); // calculate percentage of primes
         System.out.println("There are " + count + " primes between 2 and " + n + " (" + percent + "% are primes)");
     }
 }
